622189588694bcdddd8299db1e627d0bbf568eb6
Silberlachs <35577082+Silberlachs@users.noreply.github.com>
2018-01-30 18:08:42 +0100

    Add files via upload
    Pin Klasse für NeoPixel @Leeroy

---
+++ Pin.cpp
@@ -0,0 +1,29 @@
+#include "Pin.h"
+
+int pin_;
+int mode_;
+
+// konstruktor
+Pin::Pin(int p, int m) 
+{
+  pin_ = p;
+  mode_ = m;
+  pinMode(pin_, mode_);
+}
+
+// destruktor
+Pin::~Pin(void) 
+{}
+
+// wert v auf pin schreiben
+void Pin::write(int v) 
+{
+  value_ = v;
+  digitalWrite(pin_, value_);
+}
+
+int Pin::getState()
+{
+  return value_;
+}
+
---
+++ Pin.h
@@ -0,0 +1,23 @@
+#ifndef _PIN_H    //make sure only one token makes it into the current page
+#define _PIN_H
+#include <Arduino.h>
+
+class Pin {
+
+// oeffentlich zugreifbare memberfunktionen
+public: 
+  Pin(int, int);                        // konstruktor normal
+  virtual ~Pin(void);                   // destruktor
+
+  int getState();           
+  void write(int);                      // wert auf pin schreiben
+
+  
+// zugriff gesperrt, attribute fuer abgeleitete klassen zugreifbar
+protected:
+  int pin_;                             // pin nummer
+  int mode_;                            // mode input oder output
+  int value_;                           // value low oder high
+};
+
+#endif
